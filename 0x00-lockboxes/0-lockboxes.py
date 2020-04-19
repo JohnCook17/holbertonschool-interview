a8cdcd473370b680ab6e9e1fd51d395522507229
John Cook <655@holbertonschool.com>
2020-04-19 01:21:12 +0000

    Lockboxes interview question

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    my_set_of_keys = set()
+    return_value = False
+    box_number = 0
+    for values in boxes:
+        if values:
+            my_set_of_keys.update(values)
+        (_, *key_numbers) = enumerate(my_set_of_keys, 0)
+        if key_numbers and box_number != 0:
+            if box_number in list(sum(key_numbers, ())):
+                return_value = True
+            else:
+                return False
+        box_number += 1
+    
+    return return_value
