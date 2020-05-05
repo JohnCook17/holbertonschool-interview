8eefede4dd98eb6fb1d6aa4b2b41a684f56528a6
John Cook <655@holbertonschool.com>
2020-05-05 23:40:13 +0000

    might be leaking but otherwise good, still need to style

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,67 +1,60 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
-void make_heap(heap_t *head, heap_t *new_node, int value)
+heap_t *make_heap(heap_t *head, heap_t *new_node, int value)
 {
-	if (head->parent)
-		make_heap(head->parent, new_node, value);
 	if (value > head->n)
 		{
 			new_node->n = head->n;
 			head->n = value;
+			return head;
 		}
 	else
+	{
 		new_node->n = value;
+		return new_node;
+	}
 }
 
-heap_t *possition_node(heap_t *head, heap_t *new_node, int value, int left_or_right)
+void possition_node(heap_t *head, heap_t *new_node, int value)
 {
 	heap_t *new_head;
+
 	new_head = head;
+	
 	if (new_head->left == NULL)
 	{
 		new_head->left = new_node;
-		make_heap(new_head, new_node, value);
+		
 		new_node->parent = new_head;
 		new_node->left = NULL;
 		new_node->right = NULL;
-		return (new_node);
 	}
 	else if (new_head->right == NULL)
 	{
 		new_head->right = new_node;
-		make_heap(new_head, new_node, value);
 		new_node->parent = new_head;
 		new_node->left = NULL;
 		new_node->right = NULL;
-		return (new_node);
 	}
 	else
 	{
 		if (new_head->parent)
-		{
 			new_head = new_head->parent->left;
-		}
 		if (!new_head->left->right)
-		{
-			possition_node(new_head->left, new_node, value, left_or_right);
-		}	
+			possition_node(new_head->left, new_node, value);
 		else
-		{
-			possition_node(new_head->right, new_node, value, left_or_right);
-		}	
+			possition_node(new_head->right, new_node, value);
 	}
-	return (new_node);
 }
 
 heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *new_node;
 	heap_t *head;
-	int left_or_right;
+	heap_t *pointer_to_node;
 
 	/* left_or_right: 0 for left 1 for right */
-	left_or_right = 0;
 	head = *root;
 
 	/* Step 0: make a new node if no memory avaliable return NULL */
@@ -71,12 +64,14 @@ heap_t *heap_insert(heap_t **root, int value)
 	if (head == NULL)
 	{
 		*root = new_node;
+		head = new_node;
 		new_node->n = value;
 		new_node->parent = NULL;
 		new_node->left = NULL;
 		new_node->right = NULL;
-		return new_node;
 	}
 	else
-		return (possition_node(head, new_node, value, left_or_right));
+		possition_node(head, new_node, value);
+	pointer_to_node = make_heap(head, new_node, value);
+	return (pointer_to_node);
 }
