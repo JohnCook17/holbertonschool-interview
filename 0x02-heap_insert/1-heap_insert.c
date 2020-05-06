8c793a97744f7793d84e72153222650b81c58924
John Cook <655@holbertonschool.com>
2020-05-06 01:23:43 +0000

    need to work on make heap

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,34 +1,48 @@
 #include <stdlib.h>
 #include "binary_trees.h"
-
-heap_t *make_heap(heap_t *head, heap_t *new_node, int value)
+/**
+ * make_heap - makes into max heap, needs some work :(
+ * @new_node: the new node to evaluate
+ * @value: the value of the new node
+ * Return: a pointer to the node that contains the original value
+ */
+heap_t *make_heap(heap_t *new_node, int value)
 {
-	if (value > head->n)
-		{
-			new_node->n = head->n;
-			head->n = value;
-			return head;
-		}
+	int temp = new_node->parent->n;
+
+	if (value > new_node->parent->n)
+	{
+		new_node->parent->n = value;
+		new_node->n = temp;
+		return (new_node->parent);
+	}
 	else
 	{
 		new_node->n = value;
-		return new_node;
+		return (new_node);
 	}
 }
-
-void possition_node(heap_t *head, heap_t *new_node, int value)
+/**
+ * possition_node - positions the node in the heap
+ * @head: the head of the nodes we are working with, not always the root
+ * @new_node: the new node to insert into the heap
+ * @value: the value of the new node
+ * Return: same as above
+ */
+heap_t *possition_node(heap_t *head, heap_t *new_node, int value)
 {
 	heap_t *new_head;
+	heap_t *node_pointer;
 
 	new_head = head;
-	
+	node_pointer = make_heap(new_node, value);
 	if (new_head->left == NULL)
 	{
 		new_head->left = new_node;
-		
 		new_node->parent = new_head;
 		new_node->left = NULL;
 		new_node->right = NULL;
+		return (node_pointer);
 	}
 	else if (new_head->right == NULL)
 	{
@@ -36,23 +50,29 @@ void possition_node(heap_t *head, heap_t *new_node, int value)
 		new_node->parent = new_head;
 		new_node->left = NULL;
 		new_node->right = NULL;
+		return (node_pointer);
 	}
 	else
 	{
 		if (new_head->parent)
 			new_head = new_head->parent->left;
 		if (!new_head->left->right)
-			possition_node(new_head->left, new_node, value);
+			return (possition_node(new_head->left, new_node, value));
 		else
-			possition_node(new_head->right, new_node, value);
+			return (possition_node(new_head->right, new_node, value));
 	}
+	return (node_pointer);
 }
-
+/**
+ * heap_insert - inserts a node into a heap
+ * @root: the root of the entire heap
+ * @value: the value to insert
+ * Return: the node that contains the new value
+ */
 heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *new_node;
 	heap_t *head;
-	heap_t *pointer_to_node;
 
 	/* left_or_right: 0 for left 1 for right */
 	head = *root;
@@ -69,9 +89,8 @@ heap_t *heap_insert(heap_t **root, int value)
 		new_node->parent = NULL;
 		new_node->left = NULL;
 		new_node->right = NULL;
+		return (new_node);
 	}
 	else
-		possition_node(head, new_node, value);
-	pointer_to_node = make_heap(head, new_node, value);
-	return (pointer_to_node);
+		return (possition_node(head, new_node, value));
 }
