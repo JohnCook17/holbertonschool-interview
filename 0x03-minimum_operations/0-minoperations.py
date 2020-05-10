ae13289c477cce079d253d8e652d772ae302d63c
JohnCook17 <john@Johns-Laptop.localdomain>
2020-05-10 16:03:46 -0700

    first commit

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""calculates the min number of operations needed to reach a number"""
+
+
+def minOperations(n):
+    """uses prime numbers to calculate the minimum operations of n"""
+    if n < 1:
+        return 0
+    ops = []
+    while n % 2 == 0:
+        n = n // 2
+        ops.append(2)
+    for i in range(3, (int((n ** .5)) + 1), 2):
+        while n % i == 0:
+            ops.append(i)
+            n = n // i
+    ops.append(n)
+    return sum(ops)
