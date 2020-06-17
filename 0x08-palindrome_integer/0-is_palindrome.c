4203bf3675d6e6d1e4ec5a8e5258468fd203cc44
John Cook <655@holbertonschool.com>
2020-06-17 18:02:53 +0000

    0-is_palindrome.c

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "palindrome.h"
+
+
+/**
+ * itoa - takes an int and changes it to a str
+ * @val: the value to change to str
+ * @base: the number base
+ * Return: the buffer
+ */
+char *itoa(int val, int base)
+{
+	static char buf[32] = {0};
+
+	int i = 30;
+
+	for (; val && i; --i, val /= base)
+		buf[i] = "0123456789abcdef"[val % base];
+
+	return (&buf[i + 1]);
+}
+
+
+/**
+ * is_palindrome - determines if a number is a palindrome
+ * @n: the number to check
+ * Return: 0 if not 1 if so
+ */
+int is_palindrome(unsigned long n)
+{
+	char *n_str;
+	int length_n;
+	int start_n = 0;
+
+	n_str = itoa(n, 10);
+	length_n = strlen(n_str);
+
+	while (start_n != length_n)
+	{
+		printf("%c\n", n_str[start_n]);
+		printf("%c\n", n_str[length_n - 1]);
+		if (n_str[start_n] != n_str[length_n - 1])
+		{
+			return (0);
+		}
+		start_n++;
+		length_n--;
+	}
+	return (1);
+}
