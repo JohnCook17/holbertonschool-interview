be887804d2ed4d12c71e90e7876a42bcc09f8540
JohnCook17 <john@Johns-Laptop.localdomain>
2020-07-08 21:27:53 -0700

    added all functions to be safe

--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -17,6 +17,10 @@ typedef struct Grid
 	int xb, xe, yb, ye;
 } grid;
 
+void hollow(grid g);
+void carpet(grid g, int iter);
+int my_pow(int base, int expo);
+int allocate_g(char ***g, int n, const char sep);
 void menger(int level);
 
 #endif /* MENGER_H */
