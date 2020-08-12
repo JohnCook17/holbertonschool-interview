1afc67728399d3691a84a37ffbff8bdc76058ccf
JohnCook17 <jcook0017@gmail.com>
2020-08-12 18:53:19 +0000

    betty style

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -2,31 +2,41 @@
 #include <stdio.h>
 #include "search.h"
 
+/**
+ * linear_skip - a linear express lane search
+ * @list: a list to search
+ * @value: a value to search for
+ * Return: NULL on failure, else pointer to node with value
+ */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *current;
-    skiplist_t *prev;
+	skiplist_t *current;
+	skiplist_t *prev;
 
-    if(list == NULL)
-    {
-        return(NULL);
-    }
-    current = list;
-    while(current->express && current->express->n < value)
-    {
-        printf("Value checked at index [%zu] = [%d]\n", current->express->index, current->express->n);
-        prev = current;
-        current = current->express;
-    }
-    printf("Value found between indexes [%zu] and [%zu]\n", prev->index, current->index);
-    while(current->next && current->next->n < value)
-    {
-        printf("Value checked at index [%zu] = [%d]\n", current->next->index, current->next->n);
-        current = current->next;
-    }
-    printf("Value checked at index [%zu] = [%d]\n", current->next->index, current->next->n);
-    if(current && current->next && current->next->n == value)
-        return(current->next);
-    else
-        return(NULL);
+	if (list == NULL)
+	{
+		return (NULL);
+	}
+	current = list;
+	while (current->express && current->express->n < value)
+	{
+		printf("Value checked at index [%zu] = [%d]\n",
+		       current->express->index, current->express->n);
+		prev = current;
+		current = current->express;
+	}
+	printf("Value found between indexes [%zu] and [%zu]\n",
+	       prev->index, current->index);
+	while (current->next && current->next->n < value)
+	{
+		printf("Value checked at index [%zu] = [%d]\n",
+		       current->next->index, current->next->n);
+		current = current->next;
+	}
+	printf("Value checked at index [%zu] = [%d]\n",
+	       current->next->index, current->next->n);
+	if (current && current->next && current->next->n == value)
+		return (current->next);
+	else
+		return (NULL);
 }
