a95acbdc11d358589964bdc02a57f92619eb666a
JohnCook17 <jcook0017@gmail.com>
2020-08-12 18:45:41 +0000

    actual c file

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "search.h"
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+    skiplist_t *current;
+    skiplist_t *prev;
+
+    if(list == NULL)
+    {
+        return(NULL);
+    }
+    current = list;
+    while(current->express && current->express->n < value)
+    {
+        printf("Value checked at index [%zu] = [%d]\n", current->express->index, current->express->n);
+        prev = current;
+        current = current->express;
+    }
+    printf("Value found between indexes [%zu] and [%zu]\n", prev->index, current->index);
+    while(current->next && current->next->n < value)
+    {
+        printf("Value checked at index [%zu] = [%d]\n", current->next->index, current->next->n);
+        current = current->next;
+    }
+    printf("Value checked at index [%zu] = [%d]\n", current->next->index, current->next->n);
+    if(current && current->next && current->next->n == value)
+        return(current->next);
+    else
+        return(NULL);
+}
