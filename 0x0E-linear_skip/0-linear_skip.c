9ff5e9d90b2040bd5105e1916c8c4cc18df83359
JohnCook17 <jcook0017@gmail.com>
2020-08-13 15:48:58 +0000

    0-linear_skip

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -11,7 +11,8 @@
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *current;
-	skiplist_t *prev;
+	skiplist_t *last_e;
+	skiplist_t *currentp;
 
 	if (list == NULL)
 	{
@@ -22,29 +23,40 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	{
 		printf("Value checked at index [%zu] = [%d]\n",
 		       current->express->index, current->express->n);
-		prev = current;
 		current = current->express;
 	}
-	printf("Value found between indexes [%zu] and [%zu]\n",
-	       prev->index, current->index);
-	while (current->next && current->next->n < value)
+	if (current->express)
 	{
-		printf("Value checked at index [%zu] = [%d]\n",
-		       current->next->index, current->next->n);
-		current = current->next;
-	}
-	if (current->next)
-	{
-		printf("Value checked at index [%zu] = [%d]\n",
-			current->next->index, current->next->n);
+		printf("Value found between indexes [%zu] and [%zu]\n",
+			   current->index, current->express->index);
 	}
 	else
 	{
-		return (NULL);
+		last_e = current;
+		currentp = current;
+		while (currentp->next)
+		{
+			currentp = currentp->next;
+		}
+		printf("Value found between indexes [%zu] and [%zu]\n",
+			   last_e->index, currentp->index);
+	}
+	while (current->next && current->next->n < value)
+	{
+		printf("Value checked at index [%zu] = [%d]\n",
+		       current->index, current->n);
+		current = current->next;
 	}
 
+	printf("Value checked at index [%zu] = [%d]\n",
+		   current->index, current->n);
+
 	if (current && current->next && current->next->n == value)
+	{
+		printf("Value checked at index [%zu] = [%d]\n",
+		       current->next->index, current->next->n);
 		return (current->next);
+	}
 	else
 		return (NULL);
 }
