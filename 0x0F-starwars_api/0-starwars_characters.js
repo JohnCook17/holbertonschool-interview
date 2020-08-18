2c2fd4f559138a751d73482cba80a6336d389269
JohnCook17 <john@Johns-Laptop.localdomain>
2020-08-18 16:43:19 -0700

    fixed some semistandard issues

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
 
-let request = require('request');
+const request = require('request');
 
-let myArgs = process.argv.slice(2, 3);
+const myArgs = process.argv.slice(2, 3);
 const options = {
   url: 'https://swapi-api.hbtn.io/api/films/' + myArgs,
   method: 'GET'
 };
 
-if (Number.isInteger(parseInt(myArgs, 10)))
-{
+if (Number.isInteger(parseInt(myArgs, 10))) {
   request(options, (err, res, body) => {
     if (err) {
       return console.log(err);
     }
-    let obj = JSON.parse(body);
+    const obj = JSON.parse(body);
     for (let i = 0; i < obj.characters.length; i++) {
       request(obj.characters[i], (err, res, body) => {
         if (err) {
