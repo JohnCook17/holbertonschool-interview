7f776b0c23ee6dcecbb970d27ac52a2829ffcccb
JohnCook17 <john@Johns-Laptop.localdomain>
2020-08-18 16:18:08 -0700

    dont use vargit add 0-starwars_characters.js !

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-var request = require('request');
+let request = require('request');
 
-var myArgs = process.argv.slice(2, 3);
+let myArgs = process.argv.slice(2, 3);
 const options = {
   url: 'https://swapi-api.hbtn.io/api/films/' + myArgs,
   method: 'GET'
@@ -12,8 +12,8 @@ request(options, (err, res, body) => {
   if (err) {
     return console.log(err);
   }
-  var obj = JSON.parse(body);
-  for (var i = 0; i < obj.characters.length; i++) {
+  let obj = JSON.parse(body);
+  for (let i = 0; i < obj.characters.length; i++) {
     request(obj.characters[i], (err, res, body) => {
       if (err) {
         return console.log(err);
