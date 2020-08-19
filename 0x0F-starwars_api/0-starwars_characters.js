663a72a57c15a25ff33425cf07cd945fd3560efd
JohnCook17 <john@Johns-Laptop.localdomain>
2020-08-18 19:27:10 -0700

    should work

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,25 +2,40 @@
 
 const request = require('request');
 
-const myArgs = process.argv.slice(2, 3);
-const options = {
-  url: 'https://swapi-api.hbtn.io/api/films/' + myArgs,
-  method: 'GET'
+function luke (obj, i) {
+  return new Promise((resolve, reject) => {
+    request(obj.characters[i], (err, res, body) => {
+      if (err) {
+        reject(err);
+        return console.log(err);
+      }
+      resolve(JSON.parse(body).name);
+    })
+  });
 };
 
-if (Number.isInteger(parseInt(myArgs, 10))) {
-  request(options, (err, res, body) => {
-    if (err) {
-      return console.log(err);
-    }
-    const obj = JSON.parse(body);
-    for (let i = 0; i < obj.characters.length; i++) {
-      request(obj.characters[i], (err, res, body) => {
-        if (err) {
-          return console.log(err);
-        }
-        console.log(JSON.parse(body).name);
-      });
-    }
-  });
+function obiwan () {
+  const request = require('request');
+
+  const myArgs = process.argv.slice(2, 3);
+  const options = {
+    url: 'https://swapi-api.hbtn.io/api/films/' + myArgs,
+    method: 'GET'
+  };
+
+  if (Number.isInteger(parseInt(myArgs, 10))) {
+    request(options, async (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      }
+      const obj = JSON.parse(body);
+      //console.log(obj);
+      for (let i = 0; i < obj.characters.length; i++) {
+        let character = await luke(obj, i);
+        console.log(character);
+      }
+    });
+  }
 }
+
+obiwan();
