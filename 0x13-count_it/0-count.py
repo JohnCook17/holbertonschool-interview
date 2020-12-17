5e0b3108d53b37fb7d245ec1abfc730a2526816b
JohnCook17 <jcook0017@gmail.com>
2020-12-17 02:09:39 +0000

    pep8

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -2,23 +2,27 @@
 """BEHOLD MY ABOMINATION"""
 import requests
 
+
 def count_words(subreddit, word_list, *args):
     """no loops T_T"""
     # print(args)
     base_url = "https://www.reddit.com"
 
-    data = {"grant_type": "password", "username": "jcook0017", "password": "temppassword"}
+    data = {"grant_type": "password",
+            "username": "jcook0017",
+            "password": "temppassword"}
 
-    auth = requests.auth.HTTPBasicAuth("LMsd7o4qaDp3cw", "KQLYhsl5_Oe1RzvKF-77sqpEYHgGvg")
+    auth = requests.auth.HTTPBasicAuth("LMsd7o4qaDp3cw",
+                                       "KQLYhsl5_Oe1RzvKF-77sqpEYHgGvg")
 
     try:
         r = args[0]
     except IndexError:
         # print("post")
         r = requests.post(base_url + "/api/v1/access_token",
-                        data=data,
-                        headers={"user-agent": "0x13 Count it by jcook0017"},
-                        auth=auth)           
+                          data=data,
+                          headers={"user-agent": "0x13 Count it by jcook0017"},
+                          auth=auth)
 
     d = r.json()
 
@@ -28,14 +32,16 @@ def count_words(subreddit, word_list, *args):
 
     base_url = "https://oauth.reddit.com"
 
-    headers = {"Authorization": token, "User-Agent": "0x13 Count it by jcook0017"}
+    headers = {"Authorization": token,
+               "User-Agent": "0x13 Count it by jcook0017"}
 
     try:
         result = args[1]
     except IndexError:
         # print("get")
         payload = {}
-        result = requests.get(base_url + "/r/{}/hot.json".format(subreddit), headers=headers)
+        result = requests.get(base_url + "/r/{}/hot.json".format(subreddit),
+                              headers=headers)
 
     # init vars if not
     try:
@@ -58,10 +64,12 @@ def count_words(subreddit, word_list, *args):
     # print(word_list[count_word_list])
 
     if count_title < 25 and not isinstance(my_dict, list):
-        my_stirng = result.json()["data"]["children"][count_title]["data"]["title"].lower()
+        my_stirng = (result
+                     .json()["data"]["children"][count_title]["data"]["title"]
+                     .lower())
         # print("my_stirng = ", my_stirng)
         my_stirng = my_stirng.lower()
-        
+
         # print(result.status_code)
         if result.status_code == 200 and count_word_list < len(word_list):
             key = word_list[count_word_list].lower()
@@ -74,18 +82,38 @@ def count_words(subreddit, word_list, *args):
                 except KeyError:
                     my_dict[key] = my_stirng.count(key)
                 count_title += 1
-                return count_words(subreddit, word_list, r, result, my_dict, count_word_list, count_title)
+                return count_words(subreddit,
+                                   word_list,
+                                   r,
+                                   result,
+                                   my_dict,
+                                   count_word_list,
+                                   count_title)
             else:
                 count_word_list += 1
-                return count_words(subreddit, word_list, r, result, my_dict, count_word_list, count_title)
+                return count_words(subreddit,
+                                   word_list,
+                                   r,
+                                   result,
+                                   my_dict,
+                                   count_word_list,
+                                   count_title)
         # print("full 'loop'")
         count_word_list = 0
         count_title += 1
-        return count_words(subreddit, word_list, r, result, my_dict, count_word_list, count_title)
+        return count_words(subreddit,
+                           word_list,
+                           r,
+                           result,
+                           my_dict,
+                           count_word_list,
+                           count_title)
     else:
         if not isinstance(my_dict, list):
             # print(my_dict)
-            my_dict = sorted(my_dict.items(), key=lambda x:(x[1], x[0]), reverse=True)
+            my_dict = sorted(my_dict.items(),
+                             key=lambda x: (x[1], x[0]),
+                             reverse=True)
             # print(my_dict)
         try:
             list_count = args[5]
@@ -94,7 +122,11 @@ def count_words(subreddit, word_list, *args):
         if list_count < len(my_dict):
             print(my_dict[list_count][0], ": ", my_dict[list_count][1])
             list_count += 1
-            return count_words(subreddit, word_list, r, result, my_dict, count_word_list, count_title, list_count)
-
-        
-
+            return count_words(subreddit,
+                               word_list,
+                               r,
+                               result,
+                               my_dict,
+                               count_word_list,
+                               count_title,
+                               list_count)
