38afdfd44571206108f48212c3687a641cb84be6
JohnCook17 <655@holbertonschool.com>
2021-02-24 17:20:41 -0800

    tries to make exact change given a list of denominations, will return -1 on failure

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A change making program for money"""
+
+
+def makeChange(coins, total):
+    """tries to give exact change given a list of coins with a specified values
+       if unable to do so returns -1"""
+    coins.sort(reverse=True)
+
+    new_total = total
+    total_change = 0
+    total_value = 0
+
+    for coin in coins:
+        current_change = 0
+        if coin == new_total:
+            current_change = new_total // coin
+            total_value += current_change * coin
+            total_change += current_change
+        elif total % coin > 0:
+            current_change = new_total // coin
+            new_total = new_total % coin
+            total_value += current_change * coin
+            total_change += current_change
+
+    if total == total_value:
+        return total_change
+    else:
+        return - 1
---
+++ 0x19-making_change/README.md
@@ -0,0 +1,3 @@
+# 0x19. Making Change
+
+A program that tries to make exact change given a list of coins, if unable to do so returns -1.
