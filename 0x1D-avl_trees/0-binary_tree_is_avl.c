d5d63ffe5851d271341278fd6006c5c6af623416
JohnCook17 <655@holbertonschool.com>
2021-03-24 14:03:45 -0700

    style

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -23,11 +23,12 @@ int max(int a, int b)
 int height(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return 0;
+		return (0);
 
-	return 1 + max(height(node->left), height(node->right));
+	return (1 + max(height(node->left), height(node->right)));
 }
-/** isBalanced - checks if tree is avl
+/**
+ * isBalanced - checks if tree is avl
  * @root: the root of the tree to check
  * Return 0 if not 1 if so
  */
@@ -37,12 +38,12 @@ bool isBalanced(const binary_tree_t *root)
 	int rh;
 
 	if (root == NULL)
-		return 1;
+		return (1);
 	lh = height(root->left);
 	rh = height(root->right);
 	if (abs(lh - rh) <= 1 && isBalanced(root->left) && isBalanced(root->right))
-		return 1;
-	return 0;
+		return (1);
+	return (0);
 }
 /**
  * binary_tree_is_avl - checks if tree exist and if avl
